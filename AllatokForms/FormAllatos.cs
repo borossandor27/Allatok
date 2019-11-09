3ccf9d09f638807ffbb92dfb7136ecdf820bb2dd
borossandor27 <boros.sanyi@gmail.com>
2019-11-09 21:24:02 +0100

    Órán felvetett javítások

--- AllatokForms/FormAllatos.cs
+++ AllatokForms/FormAllatos.cs
@@ -16,11 +16,18 @@ namespace AllatokForms
         int OsszesAllat = 0;
         int OsszesKacsa = 0;
         int OsszesKutya = 0;
+
+        //-- a listában kiválasztott egyedek tárolására
         Kutya kivalasztottKutya;
         Kacsa kivalasztottKacsa;
+
+        //-- A listában az egyedeknek csak néhány tulajdonsága látszik, ezért listában is tároljuk őket
         List<Kutya> kutyak = new List<Kutya>();
         List<Kacsa> kacsak = new List<Kacsa>();
+
+        //-- nehogymá kéteszer kellejen beírnom! ---
         string FajlFilter = "Pontosvesszővel tagolt szövegfájl (*.csv)|*.csv|Egyszerű szövegfájl (*.txt)|*.txt|All files (*.*)|*.*";
+
         public FormAllatos()
         {
             InitializeComponent();
@@ -34,22 +41,23 @@ namespace AllatokForms
         }
 
         /// <summary>
-        /// A beviteli mezők adatai alapján új egyedet hoz létre
+        ///     A beviteli mezők adatai alapján új egyedet hoz létre
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
+        /// <param name="sender">Az eseményt kiváltó objektum</param>
+        /// <param name="e">Az esemény jellemzői</param>
         private void button_Kutyat_hozzaad_Click(object sender, EventArgs e)
         {
+            //-- kiolvassuk a beviteli mezőkből az egyedet jellemző adatokat --------------
             int kora = (int)numeric_Kutya_Eletkor.Value;
             bool ivarerett = false;
-            if (checkBox_Kutya_Ivarerett.Checked)
-            {
-                ivarerett = true;
-            }
+                ivarerett = checkBox_Kutya_Ivarerett.Checked;
             bool neme = radioButton_Kutya_Him.Checked ? true : false;
-
             string fajta = comboBox_Kutya_Fajta.SelectedItem.ToString();
+
+            //-- adatok alapján új egyed létrehozása --------------------------------------
             Kutya uj = new Kutya(kora, ivarerett, neme, fajta);
+
+            //-- Ellenőrzöm, hogy szerepel-e már ezekkel az adatokkal jellemzett egyed ----
             if (kutyak.Exists(x => x.Kora==kora && x.Ivarerett == ivarerett && x.Nem==neme && x.Fajta.Equals(fajta)))
             {
                 DialogResult valasz = MessageBox.Show("A listában már szerepel ilyen egyed!\nMégis felveszi a listába?", "Figyelem!", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
@@ -58,16 +66,25 @@ namespace AllatokForms
                     return;
                 }
             }
+            //-- Új egyedet láthatóvá tesszük a lista segítségével -------------------------
             listBox_Kutyak.Items.Add(uj.ToString());
-            OsszesKutya = listBox_Kutyak.Items.Count;
-            OsszesAllat = OsszesKutya + OsszesKacsa;
+
+            //-- Az új egyedet felvesszük a listába ----------------------------------------
             kutyak.Add(uj);
+
             Kiirja_Kozepre_A_Peldanyszamokat();
         }
 
-
+        /// <summary>
+        ///     A statisztikai adatokat megjeleníti a középső panelen ----------------------
+        /// </summary>
         private void Kiirja_Kozepre_A_Peldanyszamokat()
         {
+            //-- Aktualizáljuk a statisztikai adatokat -------------------------------------
+            OsszesKutya = listBox_Kutyak.Items.Count;
+            OsszesKacsa = listBox_Kacsak.Items.Count;
+            OsszesAllat = OsszesKacsa + OsszesKutya;
+
             label_Osszes_allat.Text = "Összesen " + OsszesAllat + " állatpéldány";
             Panel_Kozepere(label_Osszes_allat);
             label_Kutyafalka.Text = "A falkában " + OsszesKutya + " kutya van";
@@ -75,17 +92,22 @@ namespace AllatokForms
             label_KacsakSzama.Text = "Kacsák száma " + OsszesKacsa + " db";
             Panel_Kozepere(label_KacsakSzama);
         }
+
+        /// <summary>
+        ///     A középső panelen vízszintesen középre igazítja a kapott szöveget
+        /// </summary>
+        /// <param name="cimke">Az igazítandó szöveg</param>
         private void Panel_Kozepere(Label cimke)
         {
-            int w = cimke.Parent.Width;
-            cimke.Left = (w - cimke.Width) / 2;
+            int w = cimke.Parent.Width; //-- a középső panel pillanatnyi szélessége pixelben 
+            cimke.Left = (w - cimke.Width) / 2; //-- a címke relatív távolsága a panel bal szélétől pixelben
         }
 
         /// <summary>
-        /// A beviteli mezők adatai alapján új egyedet hoz létre
+        ///     A beviteli mezők adatai alapján új egyedet hoz létre
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
+        /// <param name="sender">Az eseményt kiváltó objektum</param>
+        /// <param name="e">Az esemény jellemzői</param>
         private void button_Kacsat_hozzaad_Click(object sender, EventArgs e)
         {
             int kora = (int)numeric_Kacsa_Kora.Value;
@@ -97,8 +119,6 @@ namespace AllatokForms
             bool neme = radioButton_Kacsa_Him.Checked ? true : false;
             Kacsa uj = new Kacsa(kora, ivarerett, neme, comboBox_Kacsa_tollazat.SelectedItem.ToString());
             listBox_Kacsak.Items.Add(uj.ToString());
-            OsszesKacsa = listBox_Kacsak.Items.Count;
-            OsszesAllat = OsszesKacsa + OsszesKutya;
             Kiirja_Kozepre_A_Peldanyszamokat();
             kacsak.Add(uj);
         }
@@ -106,13 +126,23 @@ namespace AllatokForms
         /// <summary>
         /// A listában kiválasztott elem adataival aktualizálja a többi objektumot. Az így kijelölt egyed a listából eltávolíthatóvá válik.
         /// </summary>
-        /// <param name="sender"></param>
+        /// <param name="sender">Az eseményt kiváltó objektum</param>
         /// <param name="e"></param>
         private void listBox_Kutyak_SelectedIndexChanged(object sender, EventArgs e)
         {
+            //-- A listában való kijelölés teszi lehetővé a törlést -------------------------------
             string[] sor = listBox_Kutyak.SelectedItem.ToString().Split();
             int kutyaID = int.Parse(sor[0]);
             kivalasztottKutya = kutyak.Find(x => x.KutyaID == kutyaID);
+            KutyadatokUpdate();
+        }
+
+        private void KutyadatokUpdate()
+        {
+            numeric_Kutya_Eletkor.Value = kivalasztottKutya.Kora;
+            checkBox_Kutya_Ivarerett.Checked = kivalasztottKutya.Ivarerett;
+            comboBox_Kutya_Fajta.SelectedItem = kivalasztottKutya.Fajta;
+ 
             if (kivalasztottKutya.Nem)
             {
                 radioButton_Kutya_Him.Checked = true;
@@ -123,33 +153,41 @@ namespace AllatokForms
                 radioButton_Kutya_Him.Checked = false;
                 radioButton_Kutya_Nosteny.Checked = true;
             }
-            numeric_Kutya_Eletkor.Value = kivalasztottKutya.Kora;
-            checkBox_Kutya_Ivarerett.Checked = kivalasztottKutya.Ivarerett;
-            comboBox_Kutya_Fajta.SelectedItem = kivalasztottKutya.Fajta;
         }
 
         /// <summary>
-        /// A listában kiválasztott elem adataival aktualizálja a többi objektumot. Az így kijelölt egyed a listából eltávolíthatóvá válik.
+        ///     A listában kiválasztott elem adataival aktualizálja a többi objektumot. Az így kijelölt egyed a listából eltávolíthatóvá válik.
         /// </summary>
-        /// <param name="sender"></param>
+        /// <param name="sender">Az eseményt kiváltó objektum</param>
         /// <param name="e"></param>
         private void listBox_Kacsak_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
+            //-- első lépés a kiválasztott egyed azonosítójának a meghatározása. 
             string[] sor = listBox_Kacsak.SelectedItem.ToString().Split();
-            int kacsaID = int.Parse(sor[0]);
+            int kacsaID = int.Parse(sor[0]);    //-- A listbox sorában az első adat szóközzel határolva
+
+            //-- Az azonosító osmeretében meghatározom a kiválasztott egyedet
             kivalasztottKacsa = kacsak.Find(x => x.kacsaID == kacsaID);
-            if (kivalasztottKacsa.Nem)
-            {
-                radioButton_Kacsa_Him.Checked = true;
-            }
-            else
-            {
-                radioButton_Kacsa_Nosteny.Checked = true;
-            }
+            Kacsaadatok_Update();
+        }
+        /// <summary>
+        ///      A választásnak megfelelően aktualizálom a beviteli mezőket
+        /// </summary>
+        private void Kacsaadatok_Update()
+        {
             numeric_Kacsa_Kora.Value = kivalasztottKacsa.Kora;
             checkBox_Kacsa_Ivarerett.Checked = kivalasztottKacsa.Ivarerett;
             comboBox_Kacsa_tollazat.SelectedItem = kivalasztottKacsa.Tollazat;
+            if (kivalasztottKacsa.Nem)
+            {
+                radioButton_Kacsa_Him.Checked = true;
+                radioButton_Kacsa_Nosteny.Checked = false;
+            }
+            else
+            {
+                radioButton_Kacsa_Him.Checked = false;
+                radioButton_Kacsa_Nosteny.Checked = true;
+            }
         }
 
         private void button_Kutya_Eltavolit_Click(object sender, EventArgs e)
@@ -165,17 +203,13 @@ namespace AllatokForms
                 {
                     listBox_Kutyak.Items.Add(item.ToString());
                 }
-                OsszesKutya--;
-                OsszesAllat--;
-                GC.SuppressFinalize(kivalasztottKutya); //-- A GC külön szálon fut, ezért vélhetően nem fog lefutni futásidőben.
-                Console.WriteLine(kivalasztottKutya.Fajta);
-            }
+                Kiirja_Kozepre_A_Peldanyszamokat();            }
         }
 
         /// <summary>
         /// A Kacsák listában kiválasztott elemet eltávolítja
         /// </summary>
-        /// <param name="sender"></param>
+        /// <param name="sender">Az eseményt kiváltó objektum</param>
         /// <param name="e"></param>
         private void button_Kacsa_Eltavolit_Click(object sender, EventArgs e)
         {
@@ -183,22 +217,24 @@ namespace AllatokForms
             if (valasztas == DialogResult.Yes)
             {
                 kacsak.Remove(kivalasztottKacsa);
-                listBox_Kutyak.Items.Clear();
+                listBox_Kacsak.Items.Clear();
                 foreach (var item in kacsak)
                 {
                     listBox_Kacsak.Items.Add(item.ToString());
                 }
-                OsszesKacsa--;
-                OsszesAllat--;
-                GC.SuppressFinalize(kivalasztottKutya);
-                Console.WriteLine(kivalasztottKutya.Fajta);
+                if (kacsak.Count > 0)
+                {
+                    listBox_Kacsak.SelectedIndex = 0;
+                }
+                Kacsaadatok_Update();
+                Kiirja_Kozepre_A_Peldanyszamokat();
             }
         }
 
         /// <summary>
         /// A kutyákat adatait szövegfájlba írja
         /// </summary>
-        /// <param name="sender"></param>
+        /// <param name="sender">Az eseményt kiváltó objektum</param>
         /// <param name="e"></param>
         private void button_Kutya_Kiir_Click(object sender, EventArgs e)
         {
